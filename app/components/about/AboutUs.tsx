48cd711d3269945fecee09be0e01a977ac7e9169
Hiwot32 <demissiehiwot32@gmail.com>
2025-12-18 14:36:22 +0300

    hero about section added

---
+++ app/components/about/AboutUs.tsx
@@ -0,0 +1,56 @@
+"use client"
+import React from 'react'
+import about from './about.module.css'
+import Image from 'next/image'
+
+export default function AboutUs() {
+  return (
+    <div className={`px-10 py-37  ${about.outerSection}`}>
+        <div className='flex gap-30 mx-auto text-white items-center'>
+            <div className='w-160'>
+                <p className='text-sm font-bold'>About Us</p>
+                <p className='text-4xl pb-8'>BRINGING PEOPLE TOGETHER,
+                    ONE CUP AT A TIME</p>
+                <div className='flex gap-7 pb-6'>
+                     <Image src="/images/aboutImg/download (1).svg" alt="cup" width={35} height={35} />
+                    <p className='text-lg'><span className='text-xl font-bold'>Food Delivery</span><br />With our fast and reliable food delivery service, your favorite coffee, snacks, and treats are just a click away.</p>
+                </div>
+                <div className='flex gap-7'>
+                    <Image src="/images/aboutImg/download.svg" alt="cup" width={35} height={35} />
+                    <p className='text-lg'><span className='text-xl font-bold'>Event Elegance</span><br />
+                    Host your special moments with us! From intimate gatherings to vibrant celebrations offers.</p>
+                </div>
+                <button className='mt-13'>More About Us</button>
+            </div>
+
+            <div className='relative '>
+                <div className='relative w-[550px]  rounded-t-full overflow-hidden group'>
+               <Image 
+                    src="/images/aboutImg/imgi_4_about-us-image.jpg"
+                    alt="about us"
+                    width={550}
+                    height={1200}
+                    className=' rounded-t-full'
+                />
+                  {/* Shine overlay */}
+    <div className="absolute top-1/2 left-1/2 w-0 h-full bg-white opacity-20 rotate-12 transform     -translate-x-1/2 -translate-y-1/2
+                  transition-all duration-1000 ease-in-out
+                  group-hover:w-[150%]">
+                    </div>
+               </div>
+
+                       {/* Opening hours box */}    
+                <div className={`${about.opening} text-black`}>
+                    <Image src='/images/footerImg/download(1).svg' alt="clock" width={50} height={50} />
+                    <p className='py-4 text-xl font-bold'>Opening Hours</p>
+                    <p className='flex gap-12 '>Monday-Friday <span>09:30-7:30</span></p>
+                    <p className='flex gap-22 py-3'>Saturday <span >10:30-05:00</span></p>
+                    <p className='flex gap-24'>Sunday <span >24hrs Open</span></p>
+                </div>
+
+            </div>
+        </div>
+      
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "./components/heroSection/HeroSection";
+import AboutUs from "./components/about/AboutUs";
 
 export default function Home() {
   return (
    <>
    <HeroSection />
+   <AboutUs />
    </>
   );
 }
